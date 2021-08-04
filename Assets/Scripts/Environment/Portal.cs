f769a41baf5ad5f24661da3f20c61ceec4e8a32e
linzexin <linzexinmasterchief@gmail.com>
2021-08-04 16:01:21 +0800

    start to make teleport and viecle

---
+++ Assets/Scripts/Environment/Portal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+    public Transform target_transform;
+    public Transform object_transform;
+
+    // function used to start teleportation
+    void Teleport()
+    {
+        object_transform.position = target_transform.position;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        object_transform = collider.transform;
+        Teleport();
+    }
+}
