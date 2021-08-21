fdeff74220d977eab6c229caba742d53a75068eb
linzexin <linzexinmasterchief@gmail.com>
2021-08-21 13:00:18 +0800

    add comments and clean up

--- Assets/Scripts/Control/Mount.cs
+++ Assets/Scripts/Control/Mount.cs
@@ -9,17 +9,11 @@ public class Mount : MonoBehaviour
     public GameObject Car;
     public GameObject People;
     public Vector3 movement;
-    // Start is called before the first frame update
+
     void Start()
     {
-        Button btn = this.GetComponent<Button>();
+        // initialize
         movement = Car.transform.position - People.transform.position;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     public void onClick()
@@ -28,24 +22,32 @@ public class Mount : MonoBehaviour
         {
             IsPeopleInTheCar = !IsPeopleInTheCar;
             movement = Car.transform.position - People.transform.position;
+            // the order of disable is important
             People.GetComponent<BlockyMove>().enabled = false;
             People.GetComponent<CharacterController>().enabled = false;
             People.GetComponent<CapsuleCollider>().enabled = false;
+
             People.transform.position = Car.transform.position;
+            // need this line to sync player position with car position
+            // move the player into car last
             People.transform.parent = Car.transform;
 
+            // enable car control
             Car.GetComponent<CarControl>().enabled = true;
         }
         else
         {
             IsPeopleInTheCar = !IsPeopleInTheCar;
+            // disable car control
+            Car.GetComponent<CarControl>().enabled = false;
+            // move player out from car first
             People.transform.parent = null;
             People.transform.position = Car.transform.position - movement;
+            // the order of enable is important, be aware the order is different from disable
             People.GetComponent<CharacterController>().enabled = true;
             People.GetComponent<BlockyMove>().enabled = true;
             People.GetComponent<CapsuleCollider>().enabled = true;
 
-            Car.GetComponent<CarControl>().enabled = false;
         }
     }
 }
